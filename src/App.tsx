75e189e6ebd29babf1ae31609b4158f04dda4d56
artonio <asukhovatkin@gmail.com>
2023-02-27 18:35:28 -0500

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,276 @@
+import type { Component } from 'solid-js';
+
+import logo from './logo.svg';
+import styles from './App.module.css';
+import { Column, Table } from './components/Table/Table';
+
+const App: Component = () => {
+  const tableData = [
+    {
+      "firstName": "Savage",
+      "lastName": "Norris",
+      "age": 5
+    },
+    {
+      "firstName": "Parker",
+      "lastName": "Bryant",
+      "age": 10
+    },
+    {
+      "firstName": "Asra",
+      "lastName": "Langley",
+      "age": 5
+    },
+    {
+      "firstName": "Gomez",
+      "lastName": "Mccray",
+      "age": 0
+    },
+    {
+      "firstName": "Nico",
+      "lastName": "Nolan",
+      "age": 6
+    },
+    {
+      "firstName": "Shawn",
+      "lastName": "Foreman",
+      "age": 0
+    },
+    {
+      "firstName": "Emery",
+      "lastName": "Gould",
+      "age": 10
+    },
+    {
+      "firstName": "Riley",
+      "lastName": "Newman",
+      "age": 3
+    },
+    {
+      "firstName": "Farren",
+      "lastName": "Solomon",
+      "age": 6
+    },
+    {
+      "firstName": "Jody",
+      "lastName": "Solomon",
+      "age": 9
+    },
+    {
+      "firstName": "Hildred",
+      "lastName": "Rosales",
+      "age": 1
+    },
+    {
+      "firstName": "Shawn",
+      "lastName": "Bright",
+      "age": 4
+    },
+    {
+      "firstName": "Bailey",
+      "lastName": "Simmons",
+      "age": 4
+    },
+    {
+      "firstName": "Sage",
+      "lastName": "Case",
+      "age": 9
+    },
+    {
+      "firstName": "Campbell",
+      "lastName": "Allen",
+      "age": 10
+    },
+    {
+      "firstName": "Shawn",
+      "lastName": "Austin",
+      "age": 10
+    },
+    {
+      "firstName": "Emery",
+      "lastName": "Harding",
+      "age": 2
+    },
+    {
+      "firstName": "Hildred",
+      "lastName": "Langley",
+      "age": 7
+    },
+    {
+      "firstName": "Duffy",
+      "lastName": "Bright",
+      "age": 5
+    },
+    {
+      "firstName": "Gomez",
+      "lastName": "Case",
+      "age": 6
+    },
+    {
+      "firstName": "Jean",
+      "lastName": "Austin",
+      "age": 9
+    },
+    {
+      "firstName": "Emery",
+      "lastName": "Guthrie",
+      "age": 8
+    },
+    {
+      "firstName": "Rory",
+      "lastName": "Rosales",
+      "age": 4
+    },
+    {
+      "firstName": "Bailey",
+      "lastName": "Burke",
+      "age": 4
+    },
+    {
+      "firstName": "Jody",
+      "lastName": "Allen",
+      "age": 6
+    },
+    {
+      "firstName": "Shiloh",
+      "lastName": "Ellison",
+      "age": 2
+    },
+    {
+      "firstName": "Vasquez",
+      "lastName": "Sexton",
+      "age": 9
+    },
+    {
+      "firstName": "Jude",
+      "lastName": "Reed",
+      "age": 6
+    },
+    {
+      "firstName": "Mildred",
+      "lastName": "Black",
+      "age": 5
+    },
+    {
+      "firstName": "Campbell",
+      "lastName": "Phillips",
+      "age": 1
+    },
+    {
+      "firstName": "Riley",
+      "lastName": "Reed",
+      "age": 3
+    },
+    {
+      "firstName": "Bailey",
+      "lastName": "Webster",
+      "age": 2
+    },
+    {
+      "firstName": "Mildred",
+      "lastName": "Battle",
+      "age": 3
+    },
+    {
+      "firstName": "Parker",
+      "lastName": "Figueroa",
+      "age": 5
+    },
+    {
+      "firstName": "Shawn",
+      "lastName": "Newman",
+      "age": 4
+    },
+    {
+      "firstName": "Rory",
+      "lastName": "Harrison",
+      "age": 1
+    },
+    {
+      "firstName": "Embry",
+      "lastName": "Norris",
+      "age": 3
+    },
+    {
+      "firstName": "Bailey",
+      "lastName": "Harrison",
+      "age": 10
+    },
+    {
+      "firstName": "Savage",
+      "lastName": "Rosales",
+      "age": 9
+    },
+    {
+      "firstName": "Parker",
+      "lastName": "Hutchinson",
+      "age": 5
+    },
+    {
+      "firstName": "Bates",
+      "lastName": "Webster",
+      "age": 6
+    },
+    {
+      "firstName": "Savage",
+      "lastName": "Black",
+      "age": 1
+    },
+    {
+      "firstName": "Chance",
+      "lastName": "Allen",
+      "age": 2
+    },
+    {
+      "firstName": "Jean",
+      "lastName": "Mccray",
+      "age": 10
+    },
+    {
+      "firstName": "Duffy",
+      "lastName": "Newman",
+      "age": 3
+    },
+    {
+      "firstName": "Emery",
+      "lastName": "Solomon",
+      "age": 6
+    },
+    {
+      "firstName": "Rory",
+      "lastName": "Christensen",
+      "age": 6
+    },
+    {
+      "firstName": "Asra",
+      "lastName": "Battle",
+      "age": 8
+    },
+    {
+      "firstName": "Campbell",
+      "lastName": "West",
+      "age": 6
+    },
+    {
+      "firstName": "Bailey",
+      "lastName": "Webster",
+      "age": 4
+    }
+  ];
+
+  return (
+      <>
+        <Table
+            data={tableData}
+            globalFilter={true}
+            onSelectionChange={(value) => console.log(value)}
+        >
+          <Column header="First Name" code="firstName" />
+          <Column header="Last Name" code="lastName" />
+          <Column header="Age" code="age" />
+        </Table>
+      </>
+  );
+};
+
+export default App;
---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,130 @@
+import { children, createSignal, For } from 'solid-js';
+import styles from './Table.module.css'
+import { Renderable } from './types';
+
+export interface IBasicProps {
+	children?: any;
+}
+
+export type TableSize = 'small' | 'medium' | 'large';
+
+export interface IColumnProps extends IBasicProps {
+	header: string;
+	code: string;
+}
+
+export const Column = (props: IColumnProps) => {
+	return (
+		<>{props}</>
+	)
+}
+
+export interface ITableProps extends IBasicProps {
+	data: any[];
+	headerRenderer?: Renderable;
+	bodyRenderer?: Renderable;
+
+	onSelectionChange?: (value: any) => void
+
+	globalFilter?: boolean;
+}
+
+const DefaultTableHeaderRenderer = (props: IColumnProps[]) => {
+	return (
+		<tr>
+			<For each={props}>
+				{column => (
+					<th>{column.header}</th>
+				)}
+			</For>
+		</tr>
+	)
+}
+
+const DefaultTableBodyRenderer = (
+	props: {
+		columns: IColumnProps[],
+		data: any[],
+		onRowSelected?: (row: any) => any
+	}
+) => {
+	const { onRowSelected } = props;
+
+	const onRowClicked = (row: any): any => {
+		onRowSelected!(row)
+	}
+
+	return (
+		<For each={props.data}>
+			{person => (
+				<tr onClick={() => onRowClicked(person)}>
+					<For each={props.columns}>
+						{column => (
+							<td>{person[column.code]}</td>
+						)}
+					</For>
+				</tr>
+			)}
+		</For>
+	)
+}
+
+const DefaultTableHeaderRendererWithFilter = (props: {onChange: (value: any) => void}) => {
+	const { onChange } = props;
+
+	const changeDetected = (e: Event) => {
+		const inputVal =  (e.target as HTMLInputElement).value;
+		onChange(inputVal)
+	}
+
+	return (
+		<div class={styles.headerContainer}>
+			<input type="text" onInput={changeDetected} />
+		</div>
+	)
+}
+
+export const Table = (props: ITableProps) => {
+
+	const [tableData, setTableData] = createSignal(props.data);
+
+	// destructure only the props that will not change
+	const { headerRenderer, bodyRenderer, onSelectionChange } = props;
+	const c = children(() => props.children);
+	const columns: IColumnProps[] = c.toArray() as unknown as IColumnProps[]
+
+	const simpleSearch = (data: any[], search: string) => {
+		if (!search) {
+			setTableData(props.data)
+			return;
+		}
+		const result =  data.filter((row: any) => {
+			return Object.keys(row).some((key: string) => {
+				return row[key].toString().toLowerCase().includes(search.toLowerCase())
+			})
+		})
+		setTableData(result)
+	}
+
+	  return (
+	<>
+		<div>
+			{props.globalFilter && <DefaultTableHeaderRendererWithFilter onChange={(value => simpleSearch(tableData(), value))} />}
+		</div>
+		<div>
+			<table class={styles.sDatatable}>
+				<thead class={styles.sDatatableHead}>
+					{headerRenderer ? headerRenderer() : <DefaultTableHeaderRenderer {...columns} />}
+				</thead>
+				<tbody class={styles.sDatatableTBody}>
+					{bodyRenderer ? bodyRenderer(tableData()) : <DefaultTableBodyRenderer
+						columns={columns}
+						data={tableData()}
+						onRowSelected={onSelectionChange}
+					/>}
+				</tbody>
+			</table>
+			</div>
+	</>
+  );
+};
